e3dc47360e1478af3cba1350b8e6bbe9b28ab7ee
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2023-12-10 09:14:22 -0800

    use new solution template for day 2

---
+++ day02/src/main.rs
@@ -0,0 +1,28 @@
+// thanks to benjamin from uci rust club for this solution template
+fn main() {
+    let input = include_str!("02.txt");
+    println!("Answer to part 1: {}", part1(input));
+    println!("Answer to part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+#[test]
+fn test_part1() {
+    let input = "";
+    assert_eq!(part1(input), 0);
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+#[test]
+fn test_part2() {
+    let input = "";
+    assert_eq!(part2(input), 0);
+}
