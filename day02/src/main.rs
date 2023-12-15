4255ae26cafbcf0e97dce80cd55a339f74e93ce4
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2023-12-14 17:56:46 -0800

    day 2 part 2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 // thanks to benjamin from uci rust club for this solution template
 fn main() {
     let input = include_str!("../rsrc/input.txt");
@@ -6,9 +8,9 @@ fn main() {
 }
 
 struct Game {
-    red: u64,
-    green: u64,
-    blue: u64, 
+    red: usize,
+    green: usize,
+    blue: usize, 
 }
 
 fn parse_game(game: &str) -> Game {
@@ -16,11 +18,11 @@ fn parse_game(game: &str) -> Game {
     for pick in game.split(", ") {
         let color: Vec<_> = pick.split(' ').collect();
         if color[1] == "red" {
-            parsed_game.red = color[0].parse::<u64>().unwrap();
+            parsed_game.red = color[0].parse::<usize>().unwrap();
         } else if color[1] == "green" {
-            parsed_game.green = color[0].parse::<u64>().unwrap();
+            parsed_game.green = color[0].parse::<usize>().unwrap();
         } else if color[1] == "blue" {
-            parsed_game.blue = color[0].parse::<u64>().unwrap();
+            parsed_game.blue = color[0].parse::<usize>().unwrap();
         }
     }
     parsed_game
@@ -57,13 +59,35 @@ fn test_part1() {
     assert_eq!(part1(input), 8);
 }
 
+
+fn game_power(games: &str) -> usize {
+    let mut min_set: Game = Game {red: 0, green: 0, blue: 0};
+    for game in games.split("; ") {
+        let current_move = parse_game(game);
+        min_set.red = max(current_move.red, min_set.red);
+        min_set.green = max(current_move.green, min_set.green);
+        min_set.blue = max(current_move.blue, min_set.blue);
+
+    }
+    power(&min_set)
+}
+
+fn power(set: &Game) -> usize {
+    set.red * set.blue * set.green
+}
+
 fn part2(input: &str) -> usize {
-    0
+    let mut sum = 0;
+    for line in input.split('\n') {
+        let games: Vec<_> = line.split(": ").collect();
+        sum += game_power(games[1]);
+    }
+    sum
 }
 
 #[cfg(test)]
 #[test]
 fn test_part2() {
     let input = include_str!("../rsrc/test.txt");
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), 2286);
 }
