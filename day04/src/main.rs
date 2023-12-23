586888bdf79444e05e2e1c69f767d98c5797a297
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2023-12-22 21:09:55 -0800

    solve day 4 part 2

--- day04/src/main.rs
+++ day04/src/main.rs
@@ -34,12 +34,34 @@ fn test_part1() {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let mut cards = vec![1; input.lines().count()];
+    let mut current_card = 0;
+    for card in input.lines() {
+        let (w, m) = card.split_once(':').unwrap().1.split_once('|').unwrap();
+        let mut winning: HashSet<usize> = HashSet::new();
+        let mut mine: HashSet<usize> = HashSet::new();
+        for x in w.split(' ') {
+            if let Ok(num) = x.parse::<usize>() {
+                winning.insert(num);
+            }
+        }
+        for x in m.split(' ') {
+            if let Ok(num) = x.parse::<usize>() {
+                mine.insert(num);
+            }
+        }
+        for i in 1..winning.intersection(&mine).count() + 1 {
+            cards[current_card + i] += cards[current_card] 
+        }
+        current_card += 1;
+    }
+    cards.iter().sum()
+
 }
 
 #[cfg(test)]
 #[test]
 fn test_part2() {
     let input = include_str!("../rsrc/test.txt");
-    assert_eq!(part2(input), 0);
+    assert_eq!(part2(input), 30);
 }
