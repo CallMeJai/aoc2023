b7e2ee9de656f0ae1215eb5ea8161099bc4a19bb
Jaiveer <32986920+CallMeJai@users.noreply.github.com>
2023-12-22 21:14:47 -0800

    add boilerplate

---
+++ day05/src/main.rs
@@ -0,0 +1,28 @@
+// thanks to benjamin from uci rust club for this solution template
+fn main() {
+    let input = include_str!("../rsrc/input.txt");
+    println!("Answer to part 1: {}", part1(input));
+    println!("Answer to part 2: {}", part2(input));
+}
+
+fn part1(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+#[test]
+fn test_part1() {
+    let input = include_str!("../rsrc/test.txt");
+    assert_eq!(part1(input), 0);
+}
+
+fn part2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+#[test]
+fn test_part2() {
+    let input = include_str!("../rsrc/test.txt");
+    assert_eq!(part2(input), 0);
+}
